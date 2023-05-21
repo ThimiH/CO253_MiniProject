ac1515d024c7b333258a98749b6d1300262f7162
Thimira Hirushan <e19276@eng.pdn.ac.lk>
2023-05-21 08:29:25 +0530

    Begin

---
+++ contactManagement.c
@@ -0,0 +1,22 @@
+/*
+Contact data mangement system.
+Date:21/05/2023
+Author: Thimira Hirushan (E/19/276)
+*/
+
+#include <stdio.h>
+
+#define NAME_SIZE 100
+#define ADDRESS_SIZE 300
+#define EMAIL_SIZE 40
+
+typedef struct {
+    char* name[NAME_SIZE];
+    unsigned int number;
+    char* address[ADDRESS_SIZE];
+    char* email[EMAIL_SIZE];
+} contact;
+
+int main() {
+    return 0;
+}
---
+++ userInterface.c
@@ -0,0 +1,13 @@
+/*
+UI of the contact mangement system.
+Date:21/05/2023
+Author: Thimira Hirushan (E/19/276)
+*/
+
+#include <stdio.h>
+
+
+
+int main() {
+    return 0;
+}
